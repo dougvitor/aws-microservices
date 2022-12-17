9b329d5a587b7b0960ccaa6b31621bb09d14d667
Douglas Vitor França da Silva <douglas.vitor@gmail.com>
2022-12-17 18:01:20 -0300

    55.Criando o modelo de produtos.

---
+++ src/main/kotlin/br/com/home/awsmicroservices/model/Product.java
@@ -0,0 +1,69 @@
+package br.com.home.awsmicroservices.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"code"})
+        }
+)
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 24, nullable = false)
+    private String model;
+
+    @Column(length = 8, nullable = false)
+    private String code;
+
+    private BigDecimal price = BigDecimal.ZERO;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
