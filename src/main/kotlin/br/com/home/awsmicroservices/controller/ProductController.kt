911eb86863a7b3a754503aef748f730ef04daf71
Douglas Vitor França da Silva <douglas.vitor@gmail.com>
2023-01-02 22:03:40 -0300

    65. Criando o serviço para publicar eventos no AWS SNS

--- src/main/kotlin/br/com/home/awsmicroservices/controller/ProductController.kt
+++ src/main/kotlin/br/com/home/awsmicroservices/controller/ProductController.kt
@@ -1,7 +1,9 @@
 package br.com.home.awsmicroservices.controller
 
+import br.com.home.awsmicroservices.enums.EventType
 import br.com.home.awsmicroservices.model.Product
 import br.com.home.awsmicroservices.repository.ProductRepository
+import br.com.home.awsmicroservices.service.ProductPublisher
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/products")
-class ProductController @Autowired constructor(private val repository: ProductRepository) {
+class ProductController @Autowired constructor(
+    private val repository: ProductRepository,
+    private val productPublisher: ProductPublisher
+) {
 
     @GetMapping
     fun findAll(): Iterable<Product> = repository.findAll()
@@ -25,13 +30,25 @@ class ProductController @Autowired constructor(private val repository: ProductRe
 
     @PostMapping
     fun save(@RequestBody product: Product): ResponseEntity<Product> =
-        ResponseEntity(repository.save(product), HttpStatus.CREATED)
+        ResponseEntity(
+            repository.save(product)
+                .also {
+                    productPublisher.publishProductEvent(it, EventType.PRODUCT_CREATED, "DouGs")
+                },
+            HttpStatus.CREATED
+        )
 
     @PutMapping(path = ["/{id}"])
     fun update(@RequestBody product: Product, @PathVariable("id") id: Long): ResponseEntity<Product> =
         if (repository.existsById(id)) {
             product.id = id
-            ResponseEntity(repository.save(product), HttpStatus.OK)
+
+            ResponseEntity(
+                repository.save(product).also {
+                    productPublisher.publishProductEvent(it, EventType.PRODUCT_UPDATE, "ViTor")
+                },
+                HttpStatus.OK
+            )
         } else {
             ResponseEntity(HttpStatus.NOT_FOUND)
         }
@@ -40,6 +57,9 @@ class ProductController @Autowired constructor(private val repository: ProductRe
     fun delete(@PathVariable("id") id: Long): ResponseEntity<Product> =
         repository.findById(id).map { product: Product ->
             repository.delete(product)
+
+            productPublisher.publishProductEvent(product, EventType.PRODUCT_DELETED, "França")
+
             ResponseEntity(product, HttpStatus.OK)
         }.orElseGet { ResponseEntity(HttpStatus.NOT_FOUND) }
 
---
+++ src/main/kotlin/br/com/home/awsmicroservices/service/ProductPublisher.kt
@@ -0,0 +1,41 @@
+package br.com.home.awsmicroservices.service
+
+import br.com.home.awsmicroservices.enums.EventType
+import br.com.home.awsmicroservices.model.Envelope
+import br.com.home.awsmicroservices.model.Product
+import br.com.home.awsmicroservices.model.ProductEvent
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.model.Topic
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+
+@Service
+class ProductPublisher @Autowired constructor(
+    private val snsClient: AmazonSNS,
+    @Qualifier("productEventsTopic") private val productEventsTopic: Topic,
+    private val mapper: ObjectMapper
+) {
+    private val LOG: Logger = LoggerFactory.getLogger(ProductPublisher::class.java)
+
+    fun publishProductEvent(
+        product: Product,
+        eventType: EventType,
+        username: String
+    ){
+        val productEvent = ProductEvent(
+            productId = product.id!!,
+            code = product.code!!,
+            username = username
+        )
+
+        val envelope = Envelope(eventType = eventType, data = mapper.writeValueAsString(productEvent))
+
+        snsClient.publish(productEventsTopic.topicArn, mapper.writeValueAsString(envelope))
+    }
+
+
+}
