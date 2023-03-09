800aa40e0823a9f57b18ae2a10dbc9ddc55e8d71
Douglas Vitor França da Silva <douglas.vitor@gmail.com>
2023-03-09 13:41:59 -0300

    113.Criando as operações de pesquisa de notas fiscais

--- src/main/kotlin/br/com/home/awsmicroservices/controller/InvoiceController.kt
+++ src/main/kotlin/br/com/home/awsmicroservices/controller/InvoiceController.kt
@@ -1,14 +1,14 @@
 package br.com.home.awsmicroservices.controller
 
+import br.com.home.awsmicroservices.model.Invoice
 import br.com.home.awsmicroservices.model.UrlResponse
+import br.com.home.awsmicroservices.repository.InvoiceRepository
 import com.amazonaws.HttpMethod
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.time.Duration
 import java.time.Instant
 import java.util.*
@@ -18,10 +18,11 @@ import java.util.*
 class InvoiceController(
     @Value("\${aws.s3.bucket.invoice.name}")
     private val bucketName: String,
-    private val amazonS3: AmazonS3
+    private val amazonS3: AmazonS3,
+    private val invoceRepository: InvoiceRepository
 ) {
 
-    @GetMapping
+    @PostMapping
     fun createInvoiceUrl(): ResponseEntity<UrlResponse> {
         val expirationTime = Instant.now().plus(Duration.ofMinutes(5))
         val processId = UUID.randomUUID().toString()
@@ -37,4 +38,10 @@ class InvoiceController(
             )
         )
     }
+
+    @GetMapping
+    fun findAll(): Iterable<Invoice> = invoceRepository.findAll()
+
+    @GetMapping("/customerName")
+    fun findByCustomerName(@RequestParam customerName: String) = invoceRepository.findAllByCustomerName(customerName)
 }
